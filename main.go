a593ea07f8f8ce1040139c0ef677f36f630cdde0
Janne Vikstedt <janne.vikstedt@eficode.com>
2016-08-09 23:43:30 +0300

    New command start

--- main.go
+++ main.go
@@ -7,8 +7,12 @@ import (
 
 	"github.com/jvikstedt/alarmii/helper"
 	"github.com/jvikstedt/alarmii/models"
-	"github.com/jvikstedt/alarmii/scheduler"
 )
+
+func init() {
+	helper.SetupLogger("log/info.log")
+	models.OpenDatabase("alarmii.db")
+}
 
 var running = true
 
@@ -16,22 +20,12 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Alarmii"
 	app.Usage = ""
-	app.Action = func(c *cli.Context) error {
-		helper.SetupLogger("log/info.log")
-		models.OpenDatabase("alarmii.db")
 
-		job := models.Job{Timing: "@every 10s", Command: "echo", Arguments: []string{"{\"status\":\"200\"}"}, ExpectedResult: map[string]string{"status": "200"}}
-		job.SaveJob()
-
-		scheduler.SetupScheduler()
-		scheduler.AddSchedulable(job.Runnable())
-		scheduler.StartScheduler()
-		for running {
-		}
-		return nil
+	app.Commands = []cli.Command{
+		commandStart(),
 	}
+
 	app.Run(os.Args)
 
-	scheduler.StopScheduler()
 	models.CloseDatabase()
 }
