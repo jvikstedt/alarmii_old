c2ad4c14c3fcf5d89817c6374e7925b5c9970a3b
Janne Vikstedt <janne.vikstedt@eficode.com>
2016-08-20 19:16:49 +0300

    Refactoring JobAction.List test

--- cli/action/job_test.go
+++ cli/action/job_test.go
@@ -36,17 +36,21 @@ func (m *JobServiceMock) All() ([]models.Job, error) {
 	return args.Get(0).([]models.Job), args.Error(1)
 }
 
+var b bytes.Buffer
+var writer = bufio.NewWriter(&b)
+var jobServiceMock = new(JobServiceMock)
+var jobAction = NewJob(writer, jobServiceMock)
+
 func TestList(t *testing.T) {
-	var b bytes.Buffer
-	writer := bufio.NewWriter(&b)
-	jobServiceMock := new(JobServiceMock)
 	jobs := []models.Job{models.Job{Timing: "@every 15"}}
 	jobServiceMock.On("All").Return(jobs, nil)
-	job := NewJob(writer, jobServiceMock)
-	job.List(Context{})
+
+	jobAction.List(Context{})
 	writer.Flush()
+
 	var resultJobs []models.Job
 	json.Unmarshal(b.Bytes(), &resultJobs)
+
 	assert.Equal(t, len(jobs), len(resultJobs))
 	assert.Equal(t, jobs[0].Timing, resultJobs[0].Timing)
 }
